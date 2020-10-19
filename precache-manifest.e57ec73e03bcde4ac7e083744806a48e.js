aaa6e04db86c244e63e760888d9a4bc0e740c8e9
tergitran <tergitran@gmail.com>
2020-10-19 19:46:37 +0700

    Updates

---
+++ precache-manifest.e57ec73e03bcde4ac7e083744806a48e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ed1a2d86f7cf3db383d5c10f32ab02e",
+    "url": "/cv-application/index.html"
+  },
+  {
+    "revision": "76c414283c838775c09e",
+    "url": "/cv-application/static/css/main.d784494f.chunk.css"
+  },
+  {
+    "revision": "09f9f6a3797140480be9",
+    "url": "/cv-application/static/js/2.b5706d23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cv-application/static/js/2.b5706d23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76c414283c838775c09e",
+    "url": "/cv-application/static/js/main.a2cef6ce.chunk.js"
+  },
+  {
+    "revision": "4a69fa3c6cf210a1d2d8",
+    "url": "/cv-application/static/js/runtime-main.26e5b48e.js"
+  }
+]);
